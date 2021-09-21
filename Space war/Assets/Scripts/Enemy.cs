8b6acbda0f23f40a12d48ef74a0dc3d535a4fd84
MaciejWarmbier <maciej.warmbier@gmail.com>
2021-09-21 02:16:58 +0200

    Added UI and Scoreboard script

--- Space war/Assets/Scripts/Enemy.cs
+++ Space war/Assets/Scripts/Enemy.cs
@@ -6,10 +6,28 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
-    private void OnParticleCollision(GameObject other) {
-        
+    [SerializeField] int enemyScore = 100;
+    ScoreBoard scoreBoard;
+
+    private void Start() {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        KillEnemy();
+    }
+
+    private void ProcessHit()
+    {
+        scoreBoard.IncrementScore(enemyScore);
+    }
+    private void KillEnemy()
+    {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         Destroy(this.gameObject);
     }
+
+    
 }
